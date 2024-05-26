5b9ec666c5e1adfee6773095547c543cdc2637f4
mahmood100100 <135098525+mahmood100100@users.noreply.github.com>
2024-05-26 18:17:21 +0300

    Home page design (profile side and post side)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Layouts/Routes.tsx";
 import "@mantine/core/styles.css";
+import { router } from "./Layouts/Routes.tsx";
 import './App.css'
 
 function App() {
-
   return (
     <div className="App">
       <div className="blur" style={{ top: '-18%', right: '0' }}></div>
       <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Post from '../Post/Post.tsx';
-import {PostsData } from '../../Api/PostsApi.ts';
-import { PostType } from '../../DataTypes/PostType.ts';
 import styles from './Posts.module.css';
+import { PostsData } from '../../Api/PostsApi.ts';
+import Post from '../Post/Post.tsx';
+import { PostType } from '../../DataTypes/PostType.ts';
 
 const Posts: React.FC = () => {
   return (
-    <div className= {styles.Posts}>
+    <div className={styles.Posts}>
       {PostsData.map((post: PostType, id: number) => (
         <Post data={post} key={id} />
       ))}
